8a560b3ff9dfce9ce8778373a7c2ce51de4fada9
Joana Brito <jobritomar@gmail.com>
2022-01-22 08:04:04 +0000

    iteration 1 & 2

--- src/App.js
+++ src/App.js
@@ -2,23 +2,112 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+const FoodBox = ({name,calories,image,quantity}) =>{
+return(
+
+  <div className="box">
+  <article className="media">
+    <div className="media-left">
+      <figure className="image is-64x64">
+        <img src={image} />
+      </figure>
+    </div>
+    <div className="media-content">
+      <div className="content">
+        <p>
+          <strong>{name}</strong> <br />
+          <small>{calories} cal</small>
+        </p>
+      </div>
+    </div>
+    <div className="media-right">
+      <div className="field has-addons">
+        <div className="control">
+          <input className="input" type="number" value={quantity} />
+        </div>
+        <div className="control">
+          <button className="button is-info">
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  </article>
+</div>
+)
+}
+
+
+const FoodAddButton = () => {
+  return(
+    <div>
+    <button>Add Food</button>
+    <FoodForm></FoodForm>
+    </div>
+  )
+}
+//único prop que FoodForm deve receber é a função que faz adiciona uma comida no array de comidas
+
+const FoodForm = (props) =>{
+  const initialState = {
+    calories: 0,
+    name:"",
+    quantity:0,
+    image:""
+  }
+  const [newFood, updateFood] = React.useState(initialState)
+
+  return(
+  
+    <form className="box">
+    <article className="media">
+      <div className="media-left">
+        <figure className="image is-64x64">
+          <img src={image} />
+        </figure>
+      </div>
+      <div className="media-content">
+        <div className="content">
+          <p>
+            <input value={name} />
+            <input value= {calories} />
+          </p>
+        </div>
+      </div>
+      <div className="media-right">
+        <div className="field has-addons">
+          <div className="control">
+            <input className="input" type="number" value={quantity} />
+          </div>
+          <div className="control">
+            <button className="button is-info">
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    </article>
+  </form>
+  )
+  }
+
+function App(props) {
+  const stuff = props.foods.map((food)=>(
+     <FoodBox 
+    name={food.name} 
+    quantity={food.quantity}
+    calories={food.calories}
+     image={food.image}>
+  </FoodBox>
+  ))
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {stuff}
+
+      <FoodAddButton></FoodAddButton>    
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import json from './foods.json';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App foods={json}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
